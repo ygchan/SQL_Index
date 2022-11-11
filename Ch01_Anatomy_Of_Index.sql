69a2074f5d8fbba9732f06641f8a8fffc53606bd
George Chan <ygchan89@gmail.com>
2022-11-10 23:16:01 -0500

    Learned what is an index and what makes it "fast"

--- Ch01_Anatomy_Of_Index.sql
+++ Ch01_Anatomy_Of_Index.sql
@@ -1,6 +1,12 @@
 -- Chapter 01 Anatomy Of An Index
 
-create table my_database(
-   my_uid int
-   , my_value varchar(10)
-);
+-- "An Index makes the query fast". But often people and book just knows about
+-- that and doesn't understand details.
+
+-- An Index is a new structure in the database that is built using the 
+-- create index statement. It requires its own hard disk space and hold an 
+-- indexed version of the data. 
+
+-- Searching an index is like searching in a phone book, or back of the 
+-- book for list of dictionary. It is fast because they are organzied, instead
+-- of giving a big messy data, that you have to look through it one by one. 
