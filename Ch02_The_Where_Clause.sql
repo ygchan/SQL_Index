32543710c5071382d9b2f1cbc2bfdf85b532d3ea
George Chan <ygchan89@gmail.com>
2022-11-14 22:31:55 -0500

    Recap of chapter 1

--- Ch02_The_Where_Clause.sql
+++ Ch02_The_Where_Clause.sql
@@ -1,1 +1,6 @@
 -- The where clause
+-- Chapter 1 described the structure of index and why it might cause poor index performance.
+-- 1) Index not unquie, 2) The data aren't stored in the same "page" (not sorted?).
+-- There are 3 operations with index lookup, 1) index unique scan, 2) index range scan
+-- table access by index rowid.
+
