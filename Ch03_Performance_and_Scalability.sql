00807965952b2978c80cfa1d9dd9d8282b78f2ce
George Chan <ygchan89@gmail.com>
2022-11-27 22:58:27 -0500

    Learned the definition of scalability

--- Ch03_Performance_and_Scalability.sql
+++ Ch03_Performance_and_Scalability.sql
@@ -1,1 +1,6 @@
 -- Chapter 03: Performance and Scalability
+
+-- What is scalability?
+-- a) the ability of a system, network, process to handle a growing
+-- amount of work in a capable manner
+-- b) its ability to be enlarged to accommodate that growth.
