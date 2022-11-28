93d66b9b90ca4b4adfcb08138f9a139f22f7badf
George Chan <ygchan89@gmail.com>
2022-11-27 23:02:12 -0500

    Examined a slow and fast index example

--- Ch03_Performance_and_Scalability.sql
+++ Ch03_Performance_and_Scalability.sql
@@ -4,3 +4,13 @@
 -- a) the ability of a system, network, process to handle a growing
 -- amount of work in a capable manner
 -- b) its ability to be enlarged to accommodate that growth.
+
+-- Author posted a very interested example.
+select count(*)
+from scale_data
+where section = @my_section
+  and id2 = @my_id2;
+  
+-- Slow index is 0.029s
+-- Fast index is 0.055s
+-- Any idea what might be the problem?
