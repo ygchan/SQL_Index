793ff56f494cdd13e92b804f5c218402e7cf99a3
George Chan <ygchan89@gmail.com>
2022-11-28 23:31:02 -0500

    Added answer

--- Ch03_Performance_and_Scalability.sql
+++ Ch03_Performance_and_Scalability.sql
@@ -14,3 +14,5 @@ where section = @my_section
 -- Slow index is 0.029s
 -- Fast index is 0.055s
 -- Any idea what might be the problem?
+
+-- Answer: it was using a composite index, but id2 is after id1.
